820e116b57228127941afa94a752b301389705bd
guardianclock <evyn.baldwin@guardianclock.ca>
2024-12-10 23:06:07 -0700

    Update const.py

--- custom_components/cync_lights/const.py
+++ custom_components/cync_lights/const.py
@@ -1,3 +1,6 @@
 """Constants for the Cync Room Lights integration."""
 
 DOMAIN = "cync_lights"
+API_AUTH = "https://api.gelighting.com/v2/user_auth"
+API_REQUEST_CODE = "https://api.gelighting.com/v2/two_factor/email/verifycode"
+API_2FACTOR_AUTH = "https://api.gelighting.com/v2/user_auth/two_factor"
