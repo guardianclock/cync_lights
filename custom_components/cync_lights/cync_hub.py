a018ba77dd77214edaa6f883817482643fc6ff0c
guardianclock <evyn.baldwin@guardianclock.ca>
2024-12-13 23:30:43 -0700

    Update cync_hub.py

--- custom_components/cync_lights/cync_hub.py
+++ custom_components/cync_lights/cync_hub.py
@@ -48,7 +48,7 @@ class CyncHub:
         self.connected_devices = {home_id:[] for home_id in self.home_controllers.keys()}
         self.shutting_down = False
         self.remove_options_update_listener = remove_options_update_listener
-        self.cync_rooms = {room_id:CyncRoom(room_id,room_info,self) for room_id,room_info in user_data.get['cync_config']['rooms'].items()}
+        self.cync_rooms = {room_id:CyncRoom(room_id,room_info,self) for room_id,room_info in self.user_data.get['cync_config']['rooms'].items()}
         self.cync_switches = {device_id:CyncSwitch(device_id,switch_info,self.cync_rooms.get(switch_info['room'], None),self) for device_id,switch_info in user_data['cync_config']['devices'].items() if switch_info.get("ONOFF",False)}
         self.cync_motion_sensors = {device_id:CyncMotionSensor(device_id,device_info,self.cync_rooms.get(device_info['room'], None)) for device_id,device_info in user_data['cync_config']['devices'].items() if device_info.get("MOTION",False)}
         self.cync_ambient_light_sensors = {device_id:CyncAmbientLightSensor(device_id,device_info,self.cync_rooms.get(device_info['room'], None)) for device_id,device_info in user_data['cync_config']['devices'].items() if device_info.get("AMBIENT_LIGHT",False)}
